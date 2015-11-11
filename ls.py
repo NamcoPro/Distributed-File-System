5b0e10f34cf34543e867467a0a6005073300087d
namcopro <eduardo3991@hotmail.com>
2015-11-10 22:47:47 -0400

    What is going on?

--- ls.py
+++ ls.py
@@ -7,12 +7,13 @@
 # 	List client for the DFS
 #
 from socket import socket
+from sys import argv, exit
 
 from Packet import *
 
 def usage():
-	print """Usage: python %s <server>:<port, default=8000>""" % sys.argv[0]
-	sys.exit(0)
+	print """Usage: python %s <server>:<port, default=8000>""" % argv[0]
+	exit(0)
 
 def client(ip, port):
 	sock = socket()
@@ -25,21 +26,21 @@ def client(ip, port):
 
 	# Contacts the metadata server and ask for list of files.
 
-
+#what the fuck is going on
 if __name__ == "__main__":
 
-	if len(sys.argv) < 2:
+	if len(argv) < 2:
 		usage()
 
 	ip = None
 	port = None
-	server = sys.argv[1].split(":")
-	if len(server == 1):
+	server = argv[1].split(":")
+	if len(server) == 1:
 		ip = server[0]
 		port = 8000
-	elif len(server == 2):
+	elif len(server) == 2:
 		ip = server[0]
-		port = int server[1]
+		port = int(server[1])
 
 	if not ip:
 		usage()
