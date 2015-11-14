e0c9e5d235a0cd8fb75048acd80b4dbec22bc96d
namcopro <eduardo3991@hotmail.com>
2015-11-14 00:17:30 -0400

    Going through the handle_put function.

--- data-node.py
+++ data-node.py
@@ -13,117 +13,123 @@ import sys
 import socket
 import SocketServer
 import uuid
-import os
+import os.path
 
 def usage():
-	print """Usage: python %s <server> <port> <data path> \
-	<metadata port,default=8000>""" % sys.argv[0]
-	sys.exit(0)
+    print """Usage: python %s <server> <port> <data path> \
+    <metadata port,default=8000>""" % sys.argv[0]
+    sys.exit(0)
 
 def register(meta_ip, meta_port, data_ip, data_port):
-	"""Creates a connection with the metadata server and
-	   register as data node
-	"""
+    """Creates a connection with the metadata server and
+       register as data node
+    """
 
-	# Establish connection
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect((meta_ip, meta_port))
+    # Establish connection
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((meta_ip, meta_port))
 
-	#Wow. Everything was all here. Fucking Packets, man.
-	try:
-		response = "NAK"
-		sp = Packet()
-		while response == "NAK":
-			sp.BuildRegPacket(data_ip, data_port)
-			sock.sendall(sp.getEncodedPacket())
-			response = sock.recv(1024)
+    #Wow. Everything was all here. Fucking Packets, man.
+    try:
+        response = "NAK"
+        sp = Packet()
+        while response == "NAK":
+            sp.BuildRegPacket(data_ip, data_port)
+            sock.sendall(sp.getEncodedPacket())
+            response = sock.recv(1024)
 
-			if response == "DUP":
-				print "Duplicate Registration"
+            if response == "DUP":
+                print "Duplicate Registration"
 
-		 	if response == "NAK":
-				print "Registratation ERROR"
+            if response == "NAK":
+                print "Registratation ERROR"
 
-	finally:
-		sock.close()
+    finally:
+        sock.close()
 
 
 class DataNodeTCPHandler(SocketServer.BaseRequestHandler):
 
-	#Doubts
-	def handle_put(self, p):
+    #Doubts
+    def handle_put(self, p):
+        """Receives a block of data from a copy client, and
+           saves it with an unique ID.  The ID is sent back to the
+           copy client.
+        """
 
-		"""Receives a block of data from a copy client, and
-		   saves it with an unique ID.  The ID is sent back to the
-		   copy client.
-		"""
+        fname, fsize = p.getFileInfo()
 
-		fname, fsize = p.getFileInfo()
+        #really, what is this?
+        #self.request.send("OK")
 
-		self.request.send("OK")
-
-		# Generates an unique block id.
-		blockid = str(uuid.uuid1())
-
-
-		# Open the file for the new data block.
-
-		# Receive the data block.
-
-		# Send the block id back
+        # Generates an unique block id.
+        blockid = str(uuid.uuid1())
 
 
-	def handle_get(self, p):
+        # Open the file for the new data block.
+        path_and_name = "%s/%s_%s" % (DATA_PATH, fname, blockid)
+        block_file = open(path_and_name, "w")
 
-		# Get the block id from the packet
-		blockid = p.getBlockID()
+        # Receive the data block.
+        block = self.request.recv(1024)
+
+        #writting the block contents to the file
+        block_file.write(block)
+        block_file.close()
+
+        # Send the block id back
+        self.request.sendall(blockid)
+
+    def handle_get(self, p):
+
+        # Get the block id from the packet
+        blockid = p.getBlockID()
 
 
-		# Read the file with the block id data
-		# Send it back to the copy client.
+        # Read the file with the block id data
+        # Send it back to the copy client.
 
-		# Fill code
+        # Fill code
 
-	def handle(self):
-		msg = self.request.recv(1024)
-		print msg, type(msg)
+    def handle(self):
+        msg = self.request.recv(1024)
+        print msg, type(msg)
 
-		p = Packet()
-		p.DecodePacket(msg)
+        p = Packet()
+        p.DecodePacket(msg)
 
-		cmd = p.getCommand()
-		if cmd == "put":
-			self.handle_put(p)
+        cmd = p.getCommand()
+        if cmd == "put":
+            self.handle_put(p)
 
-		elif cmd == "get":
-			self.handle_get(p)
+        elif cmd == "get":
+            self.handle_get(p)
 
 
 if __name__ == "__main__":
 
-	META_PORT = 8000
-	if len(sys.argv) < 4:
-		usage()
+    META_PORT = 8000
+    if len(sys.argv) < 4:
+        usage()
 
-	try:
-		HOST = sys.argv[1]
-		PORT = int(sys.argv[2])
-		DATA_PATH = sys.argv[3]
+    try:
+        HOST = sys.argv[1]
+        PORT = int(sys.argv[2])
+        DATA_PATH = sys.argv[3]
 
-		if len(sys.argv) > 4:
-			META_PORT = int(sys.argv[4])
+        if len(sys.argv) > 4:
+            META_PORT = int(sys.argv[4])
 
-		if not os.path.isdir(DATA_PATH):
-			print "Error: Data path %s is not a directory." % DATA_PATH
-			usage()
-	except:
-        print "Exception."
-		usage()
+        if not os.path.isdir(DATA_PATH):
+            print "Error: Data path %s is not a directory." % DATA_PATH
+            usage()
+    except:
+        usage()
 
 
-	register("localhost", META_PORT, HOST, PORT)
-	server = SocketServer.TCPServer((HOST, PORT), DataNodeTCPHandler)
+    register("localhost", META_PORT, HOST, PORT)
+    server = SocketServer.TCPServer((HOST, PORT), DataNodeTCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
- 	server.serve_forever()
+    server.serve_forever()
